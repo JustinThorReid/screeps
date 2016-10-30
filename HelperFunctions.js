51767bee1cf4929160d8bdf28716fd406929f606
Phatness <phatness0@gmail.com>
2016-10-29 20:38:36 -0400

    helper function to create roads

---
+++ HelperFunctions.js
@@ -0,0 +1,20 @@
+/**
+ * Created by phatn on 10/29/2016.
+ */
+module.exports = {
+    createRoads: function(startPos, endPos) {
+        var path = PathFinder.search(startPos, {
+            pos:endPos,
+            range:1
+        });
+
+        for(var n in path) {
+            var pos = path[n];
+            var room = Game.rooms[pos.roomName];
+
+            if(room) {
+                room.createConstructionSite(STRUCTURE_ROAD);
+            }
+        }
+    }
+};
